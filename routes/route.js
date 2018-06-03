114428ca8fa59f9e22e212abeffe83e503dbb2c3
Squishy123 <christian.gnaw@gmail.com>
2018-06-02 20:27:48 -0400

    Init react project

--- routes/route.js
+++
@@ -1,5 +0,0 @@
-exports = module.exports = function (io) {
-    io.sockets.on('connection', function (socket) {
-        socket.on('/api', () => { return io.emit('/api', "Hello World!") });
-    })
-}
