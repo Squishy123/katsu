3ec120de69f81a8d9c92d1be110ef988990fa5f3
Christian Wang <christian.gnaw@gmail.com>
2018-06-05 13:25:28 -0400

    Rebuilt home page with bloomer ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {BrowserRouter} from 'react-router-dom';
 import Main from './main';
 import styles from './index.css';
 
+import 'bulma/css/bulma.min.css';
+
 render((
     <BrowserRouter>
         <Main />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,61 @@
 import React from 'react';
 import { render } from 'react-dom';
-import styles from './home.css';
-import * as kitsu from '../scripts/kitsu-api.js'
+//import styles from './home.css';
+import * as kitsu from '../scripts/kitsu-api.js';
+import {Container, Content, Title, Card, Tile, Box, Button, CardHeader, Subtitle, CardHeaderTitle, CardImage, Image, CardContent, Media, MediaLeft} from 'bloomer';
 
-import MediaCard from '../components/mediaCard.js';
+//import MediaCard from '../components/mediaCard.js';
 
 class Home extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
+        /*this.state = {
             trendingAnime: [<h1 className="display">Trending Anime</h1>]
-        }
+        }*/
     } 
 
     async loadTrending() {
         let items = await kitsu.getTrending();
-        let mediaList = items.data.map((e) => {
-           return <MediaCard meta={e}/>
-        });
-        this.state.trendingAnime.push(<div className="mediaList">{mediaList}</div>)
-        render(this.state.trendingAnime, document.querySelector('#trendingAnime'))
+        let mediaList = [];
+        
+        for(let i = 0; i < items.data.length; i+=4) {
+            let temp = [];
+            mediaList.push((<Tile isParent>{temp}</Tile>));
+                items.data.slice(i, i+4).forEach((e) => {
+                    temp.push(
+                    <Tile isChild isSize={3} style={{padding: "10px"}}>
+                        <Card style={{height: "100%"}}>
+                            <CardHeader>
+                                <CardHeaderTitle>
+                                 <Subtitle isSize={5}>{e.attributes.canonicalTitle}</Subtitle>
+                                </CardHeaderTitle>
+                            </CardHeader>
+                            <CardImage>
+                                <Image src={e.attributes.posterImage.medium}/>
+                            </CardImage>
+                            <CardContent>
+                                <Button href={`/animes/${e.id}/${e.attributes.canonicalTitle}`} isColor='dark'>
+                                    Watch Now
+                                </Button>
+                            </CardContent>
+                        </Card>
+                   </Tile>)
+                });    
+        }
+        console.log(mediaList);
+
+       // this.state.trendingAnime.push(<div className="mediaList">{mediaList}</div>)
+        render(mediaList, document.querySelector('#trendingList'))
     }
 
     render() {
         this.loadTrending();
         return (
-            <div>
-                <div className="jumbotron" id="trendingAnime">
-                </div>
-            </div>)
+            <Container style={{padding: "0 10px 0 10px"}}>
+                <Tile isAncestor id="trendingList" isVertical>
+                </Tile>
+            </Container>)
     }
 
 }
