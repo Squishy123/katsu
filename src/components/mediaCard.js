66a1ee75793543894f095b174312b318dcd4dd32
Squishy123 <christian.gnaw@gmail.com>
2018-06-04 23:35:04 -0400

    Added watch button with styles

--- src/components/mediaCard.js
+++ src/components/mediaCard.js
@@ -12,6 +12,7 @@ export default class MediaCard extends React.Component {
             <div className="card">
                 <img className="poster" src={this.props.meta.attributes.posterImage.medium}></img>
                 <h2 className="display">{this.props.meta.attributes.canonicalTitle}</h2>
+                <a href={`/animes/${this.props.meta.id}/${this.props.meta.attributes.canonicalTitle}`}>Watch Now</a>
             </div>
         </div>
         )
